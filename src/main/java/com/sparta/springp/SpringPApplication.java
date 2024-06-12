2c00a717a254265616707098754253f2d3108aa9
joon <tjrwns3428@gmail.com>
2024-06-12 16:37:20 +0900

    init

---
+++ src/main/java/com/sparta/springp/SpringPApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/springp/SpringPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.springp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
